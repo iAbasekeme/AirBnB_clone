4c6f7d0e082c2d2e971f1dc5a032048537bfa096
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-06 14:36:32 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,41 +5,33 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    """A BaseModel class
-    """
-    def __init__(self):        
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+	"""A BaseModel class
+	"""
+	def __init__(self):
+		"""An init method for instances
+		"""
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+		
+	def __str__(self):
+		"""A string method
 
-    def save(self):        
-        self.updated_at = datetime.now()        
-        
-        
-    def to_dict(self):                   
-        dictionary = self.__dict__.copy()
-        dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
+		Returns:
+			str: For string output
+		"""
+		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-        return dictionary
+	def save(self):    
+		"""A save method to update time
+		"""
+		self.updated_at = datetime.now()        
+		
+		
+	def to_dict(self):                   
+		dictionary = self.__dict__.copy()
+		dictionary["__class__"] = self.__class__.__name__
+		dictionary["created_at"] = self.created_at.isoformat()
+		dictionary["updated_at"] = self.updated_at.isoformat()
 
-    
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-print('---------------------------------------')
-my_model.save()
-print(my_model)
-print('---------------------------------------')
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print('---------------------------------------')
-
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+		return dictionary
