2d740a01b2aa1d5dc47fe82e5a89cbbad783ce14
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-06 14:49:36 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,40 +4,41 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    
-	"""A BaseModel class
-	"""
-	def __init__(self):
-		"""An init method for instances
-		"""
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
-		
-	def __str__(self):
-		"""A string method
 
-		Returns:
-			str: For string output
-		"""
-		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    """A BaseModel class
+    """
 
-	def save(self):
-		"""A save method to update time
-		"""
-		self.updated_at = datetime.now()        
-		
-		
-	def to_dict(self):
-		""" returns a dictionary containing all keys/values of __dict__ of the instance
+    def __init__(self):
+        """An init method for instances
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-		Returns:
-			dict: dictionary for all instances
-		"""
-		dictionary = self.__dict__.copy()
-		dictionary["__class__"] = self.__class__.__name__
-		dictionary["created_at"] = self.created_at.isoformat()
-		dictionary["updated_at"] = self.updated_at.isoformat()
+    def __str__(self):
+        """A string method
 
-		return dictionary
+        Returns:
+                str: For string output
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """A save method to update time
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of __dict__ of the instance
+
+        Returns:
+                dict: dictionary for all instances
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+
+        return dictionary
