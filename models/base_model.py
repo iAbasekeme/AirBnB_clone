934d2f65802863ccebf8c2e76e6f73a749f01ebc
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-08 08:31:33 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,22 @@ class BaseModel:
     """A BaseModel class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """An init method for instances
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            del kwargs['__class__']
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    setattr(self, k, datetime.strptime(
+                        v, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        # storage.new()
 
     def __str__(self):
         """A string method
@@ -29,6 +39,7 @@ class BaseModel:
         """A save method to update time
         """
         self.updated_at = datetime.now()
+        # storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing
@@ -43,3 +54,27 @@ class BaseModel:
         dictionary["updated_at"] = self.updated_at.isoformat()
 
         return dictionary
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+          type(my_model_json[key]), my_model_json[key]))
+
+print("-----------------------")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
