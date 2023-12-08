45038d8f59b1c747bc80c8e98fa08b0764aca477
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-08 12:18:01 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This file contains a super class BaseModel
 """
+import models
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -14,19 +14,17 @@ class BaseModel:
         """An init method for instances
         """
         if kwargs:
-            del kwargs['__class__']
             for k, v in kwargs.items():
                 if k in ['created_at', 'updated_at']:
                     setattr(self, k, datetime.strptime(
                         v, '%Y-%m-%dT%H:%M:%S.%f'))
-                else:
+                elif k != '__class__':
                     setattr(self, k, v)
 
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
         models.storage.new(self)
 
     def __str__(self):
@@ -41,8 +39,6 @@ class BaseModel:
         """A save method to update time
         """
         self.updated_at = datetime.now()
-        storage.save()
-
         models.storage.save()
 
     def to_dict(self):
@@ -58,27 +54,3 @@ class BaseModel:
         dictionary["updated_at"] = self.updated_at.isoformat()
 
         return dictionary
-
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
-
-print("-----------------------")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
