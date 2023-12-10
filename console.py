6e0d311d44d825c6bdaf247f9cab812481a1dd88
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-10 23:00:34 +0100

    removed extra spaces from the classes dictionary

--- console.py
+++ console.py
@@ -22,14 +22,14 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
     classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-            }
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+    }
 
     def emptyline(self):
         """
