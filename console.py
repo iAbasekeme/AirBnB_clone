d9b7710f39dc3be5a1d120399c4ffd8618ef8069
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2023-12-11 06:39:43 +0100

    fixed wrong error message at line 106 and 171

--- console.py
+++ console.py
@@ -103,7 +103,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
         elif arguments[0] not in HBNBCommand.classes:
-            print("** class doesn't exist**")
+            print("** class doesn't exist **")
 
         elif len(arguments) == 1 and arguments[0] in HBNBCommand.classes:
             print("** instance id missing **")
@@ -168,7 +168,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
         elif arguments[0] not in HBNBCommand.classes:
-            print("** class doesn't exist**")
+            print("** class doesn't exist **")
         elif arguments[0] in HBNBCommand.classes and len(arguments) == 1:
             print("** instance id missing **")
         elif arguments[0] in HBNBCommand.classes and len(arguments) == 2:
